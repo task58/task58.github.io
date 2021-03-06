05aaa418db5f03737d02fd69024c05d88ceac762
task58 <task58@outlook.jp>
2021-03-06 22:07:30 +0900

    09

---
+++ typingGame/play/window.js
@@ -0,0 +1,1 @@
+const electron=require("electron"),app=electron.app,BrowserWindow=electron.BrowserWindow;let mainWindow=null;app.on("ready",(()=>{mainWindow=new BrowserWindow({width:1200,height:900}),mainWindow.loadURL("file://"+__dirname+"/index.html"),mainWindow.on("closed",(function(){mainWindow=null}))}));
